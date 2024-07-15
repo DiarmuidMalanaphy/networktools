6cc4112898768ccfce8d3c044b26a7646b933946
diarmuidmalanaphy <diarmuidmalanaphy@gmail.com>
2024-07-15 03:09:56 +0100

    Fixed a bunch of bugs and added documentation for new functions

--- networktools.go
+++ networktools.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-// SendUDP takes an address and data and uses UDP to send transmit the data
+// SendUDP takes an address and data and uses UDP to send transmit the data.
 // It is advised to use the Request format provided in standards.go and serialise it using GenerateRequest. These are included within the package to make your life easier.
 //
 // Example:
@@ -56,7 +56,15 @@ func SendUDP(target_address string, data []byte) error {
 
 // SendInitialTCP is used to start a connection between two machines using TCP.
 // It works similarly to SendUDP with the distinction being this function returns a connection.
-// The function takes an address and some initial data to send and returns the established TCP connection.
+// Be aware you will have to close the connection yourself. It was chosen to defer this to the programmer so more complex exchanges could be handled.
+//
+// Example:
+//
+//	conn, err := SendInitialTCP(target_addr, data)
+//	if err != nil {
+//		return nil, fmt.Errorf("error in SendInitialTCP: %w", err)
+//	}
+//	defer conn.Close() // Ensure the connection is closed when we're done
 func SendInitialTCP(target_address string, data []byte) (net.Conn, error) {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", target_address)
 	if err != nil {
@@ -77,6 +85,15 @@ func SendInitialTCP(target_address string, data []byte) (net.Conn, error) {
 	return conn, nil
 }
 
+// Get_TCP_Reply is used get the reply on a TCP connection. The function pairs well with SendInitialTCP, which is why the function Handle_Single_TCP_Exchange is provided.
+// Something to note is that the buffer size will have to be defined based on how large you're expecting a given reply to be.
+//
+// Example:
+//
+//	buff, err := Get_TCP_Reply(conn, buff_size)
+//	if err != nil {
+//		return nil, fmt.Errorf("error in Get_TCP_Reply: %w", err)
+//	}
 func Get_TCP_Reply(conn net.Conn, buff_size uint16) ([]byte, error) {
 	conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 	buffer := make([]byte, buff_size)
@@ -105,6 +122,13 @@ func Get_TCP_Reply(conn net.Conn, buff_size uint16) ([]byte, error) {
 
 // SendTCPReply is a function to reply to a given TCP connection.
 // The function takes a given connection and data to send and returns an error value, with nil implying there has been no error.
+//
+// Example:
+//
+//	err := SendTCPReply(previous_conn, data)
+//	if err != nil {
+//		return nil, err
+//	}
 func SendTCPReply(conn net.Conn, data []byte) error {
 	if conn == nil {
 		return fmt.Errorf("connection is nil")
@@ -119,6 +143,15 @@ func SendTCPReply(conn net.Conn, data []byte) error {
 	return nil
 }
 
+// Handle_Single_TCP_Exchange handles a single exchange of TCP and then closes the connection.
+// You will have to implement more complex exchanges yourself using functions within this package.
+//
+// Example:
+//
+//	(Purposefully excluded error handling)
+//	garb := NewGarb(8)
+//	req, _ := networktools.GenerateRequest(garb, 14)
+//	data, _ := networktools.Handle_Single_TCP_Exchange("192.168.1.76:5057", req, 1024)
 func Handle_Single_TCP_Exchange(target_addr string, data []byte, buff_size uint16) ([]byte, error) {
 	conn, err := SendInitialTCP(target_addr, data)
 	if err != nil {
@@ -137,6 +170,14 @@ func Handle_Single_TCP_Exchange(target_addr string, data []byte, buff_size uint1
 // GetPublicIP is a function to get the public IP address of the machine.
 // The function takes no input and returns a string identifying the IP address.
 // It will be noted it is currently relying on a public API so in future there may be bugs / it may not work and will have to be updated.
+//
+// Example:
+//
+//	publicIP, err := GetPublicIP()
+//	if err != nil {
+//		fmt.Println("Error getting public IP:", err)
+//	}
+//	fmt.Printf("Server Global IP is - %s\n", publicIP)
 func GetPublicIP() (string, error) {
 	resp, err := http.Get("https://api.ipify.org")
 	if err != nil {
@@ -151,6 +192,14 @@ func GetPublicIP() (string, error) {
 }
 
 // GetLocalIP is a function to get the Local IP address on the machine's WiFi network.
+//
+// Example:
+//
+//	localIP, err := GetLocalIP()
+//	if err != nil {
+//	fmt.Println("Error getting Local IP:", err)
+//		}
+//	fmt.Printf("Server Local IP is - %s\n", localIP)
 func GetLocalIP() (string, error) {
 	interfaces, err := net.Interfaces()
 	if err != nil {
