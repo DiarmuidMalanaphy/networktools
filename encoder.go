2a4f6cb5b9636b765c0a54b249048972e6bc0157
diarmuidmalanaphy <diarmuidmalanaphy@gmail.com>
2024-07-24 00:27:43 +0100

    flip it

--- encoder.go
+++ encoder.go
@@ -16,19 +16,12 @@ import (
 //	_ := deserialiseData(&ic, req.Request.Payload)
 //	newCamera := (Logic to generate camera object)
 //	outgoingReq, err := generateRequest(newCamera, RequestSuccessful)
-func GenerateRequest(data interface{}, reqType uint8) ([]byte, error) {
-	// First, serialize the data
-	fmt.Println("THIS SHOULD 100% be printed")
-	serialisedData, err := __serialiseData(data)
-	fmt.Println("HereC")
-	if err != nil {
-		return nil, err
-	}
+func GenerateRequest(data []byte, reqType uint8) ([]byte, error) {
 
 	// Create a Request with the serialized data as the payload
 	req := Request_Type{
 		Type:    reqType,
-		Payload: serialisedData,
+		Payload: data,
 	}
 	serialisedRequest, err := __serialiseRequest(req)
 	if err != nil {
@@ -37,39 +30,6 @@ func GenerateRequest(data interface{}, reqType uint8) ([]byte, error) {
 
 	// Return the serialized request
 	return serialisedRequest, nil
-}
-
-func __serialiseData(data_to_be_serialised interface{}) ([]byte, error) {
-	// Get the reflect.Type of the input
-	t := reflect.TypeOf(data_to_be_serialised)
-
-	// Check if the type is a pointer, and if so, get the element type
-	if t.Kind() == reflect.Ptr {
-		t = t.Elem()
-	}
-
-	// Check if the type has a ToProto method
-	_, exists := t.MethodByName("ToProto")
-	if !exists {
-		return nil, fmt.Errorf("input does not implement ToProto() method")
-	}
-
-	// Call the ToProto method
-	v := reflect.ValueOf(data_to_be_serialised)
-	if v.Kind() == reflect.Ptr && v.IsNil() {
-		return nil, fmt.Errorf("nil pointer")
-	}
-
-	protoMsg := v.MethodByName("ToProto").Call(nil)[0].Interface()
-
-	// Convert to proto.Message
-	msg, ok := protoMsg.(proto.Message)
-	if !ok {
-		return nil, fmt.Errorf("ToProto() does not return a proto.Message")
-	}
-
-	// Marshal the protobuf message to a byte slice
-	return proto.Marshal(msg)
 }
 
 func DeserialiseData(msg proto.Message, raw_data []byte) error {
